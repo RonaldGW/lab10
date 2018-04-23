027ffee5f1dd2024ed0b0ed62db7e10368a7e7b9
Ryan Randles <randles.ryan@gmail.com>
2018-04-23 17:02:29 -0500

    Add files via upload
    Copies of what Ryan was working on today (4/23)

--- GClientA.java
+++ GClientA.java
@@ -1,4 +1,5 @@
-package lab10;
+
+package edu.uab.cs203.playground;
 import java.io.Serializable;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
@@ -7,7 +8,6 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
-
 import edu.uab.cs203.Objectmon;
 import edu.uab.cs203.ObjectmonNameGenerator;
 import edu.uab.cs203.Team;
@@ -19,93 +19,95 @@ import edu.uab.cs203.network.NetworkGym;
 
 public class GClientA extends UnicastRemoteObject implements Serializable, GymClient {
 
-
-
-protected GClientA() throws RemoteException {
+	private static final long serialVersionUID = 1L;
+	protected GClientA() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-private static Team<Objectmon> team;
-private GymServer client;
-public static void main (String[] arg0) throws RemoteException {
-	
-	try {
-	int clientPort = 9995;
-    int serverPort = 10001;
-    String serverHost = "localhost";
-    GymClient GClientA = new GClientA();
-
-    Runtime.getRuntime().exec("rmiregistry " + clientPort);
-    Registry registry = LocateRegistry.createRegistry(clientPort);
-    registry.bind("GClientA", GClientA);
-    System.out.println("one");
-    Registry serverRegistry = LocateRegistry.getRegistry(serverHost, serverPort);
-    GymServer client = (GymServer) serverRegistry.lookup("GServer");
-    System.out.println("two");
-    team = new BasicTeam<>("Team A", 6);
-    Hashmon.loadObjectdex("C:\\Users\\glenn\\eclipse-workspace\\lab10\\src\\lab10\\objectdex_01.txt");
-    for(int i=0;i<=5;i++) {
-    	team.add(new Hashmon(ObjectmonNameGenerator.nextName()));}
-    client.registerClientA("localhost", clientPort, "GClientA");
-    System.out.println("three");
-    Scanner sk = new Scanner(System.in);
-    System.out.println("Are you ready?");
-	String begin = sk.nextLine();
-	if(begin.equals("yes")) {
-		
-		client.setTeamAReady(true);
 	}
 	
-	
-	
-	}catch(Exception e) {
-		System.out.println("Trace back:");
-		e.printStackTrace();
-	}
-}
-@Override
-public Team<Objectmon> getTeam() throws RemoteException {
-	return this.team;
-}
 
-@Override
-public Objectmon networkApplyDamage(Objectmon arg0, Objectmon arg1, int arg2) throws RemoteException {
-	this.nextObjectmon().setHp((arg1.getHp()-arg2));
-	return arg1;
-}
+	private static Team<Objectmon> team;
+	private GymServer client;
+	public static void main (String[] arg0) throws RemoteException {
 
-@Override
-public void networkTick() throws RemoteException {
-	this.team.tick();
+		try {
+			int clientPort = 9995;
+			int serverPort = 10001;
+			String clienta = null;
+			String serverHost = "localhost";
+			GymClient GClientA = new GClientA();
 
-}
+			Runtime.getRuntime().exec("rmiregistry " + clientPort);
+			Registry registry = LocateRegistry.createRegistry(clientPort);
+			registry.bind("GClientA", GClientA);
+			System.out.println("one");
+			Registry serverRegistry = LocateRegistry.getRegistry(serverHost, serverPort);
+			GymServer client = (GymServer) serverRegistry.lookup("GServer");
+			System.out.println("two");
+			team = new BasicTeam<>("Team A", 6);
+			Hashmon.loadObjectdex("C:\\Users\\randl\\eclipse-workspace\\Lab10\\objectdex_01.txt");
+			for(int i=0;i<=5;i++) {
+				Hashmon.getEntry("C:\\Users\\randl\\eclipse-workspace\\Lab10\\objectdex_01.txt");
+				team.add(new Hashmon(ObjectmonNameGenerator.nextName()));}
+			client.registerClientA("localhost", clientPort, "GClientA");
+			System.out.println("three");
+			Scanner sk = new Scanner(System.in);
+			System.out.println("Are you ready?");
+			String begin = sk.nextLine();
+			if(begin.equals("yes")) {
 
-@Override
-public Objectmon nextObjectmon() throws RemoteException {
-	for(int i=0; i<team.size(); i++) {
-		if(((Objectmon) team.get(i)).isFainted()!= true) {
-			return (Objectmon) team.get(i);
+				client.setTeamAReady(true);
+				client.printMessage(clienta);
+			}
+
+		}catch(Exception e) {
+			System.out.println("Trace back:");
+			e.printStackTrace();
 		}
 	}
-	return null;
+	@Override
+	public Team<Objectmon> getTeam() throws RemoteException {
+		return this.team;
+	}
 
-}
+	@Override
+	public Objectmon networkApplyDamage(Objectmon from, Objectmon to, int damage) throws RemoteException {
+		this.nextObjectmon().setHp((to.getHp()-damage));
+		return to;
+	}
 
-@Override
-public void printMessage(String arg0) throws RemoteException {
-	System.out.print(arg0);
-	
-}
+	@Override
+	public void networkTick() throws RemoteException {
+		this.team.tick();
 
-@Override
-public void setTeam(Team arg0) throws RemoteException {
-	team = arg0;
-	return;
+	}
 
-}
-public void CreateTeam() throws RemoteException {
-    for(int i=0;i<=this.getTeam().getMaxSize();i++) {
-        Hashmon bob=new Hashmon(ObjectmonNameGenerator.nextName());
-        team.add(bob);
-    }}
+	@Override
+	public Objectmon nextObjectmon() throws RemoteException {
+		for(int i=0; i<team.size(); i++) {
+			if(((Objectmon) team.get(i)).isFainted()!= true) {
+				return (Objectmon) team.get(i);
+			}
+		}
+		return null;
+
+	}
+
+	@Override
+	public void printMessage(String message) throws RemoteException {
+		System.out.print(message);
+
+	}
+
+	@Override
+	public void setTeam(Team team) throws RemoteException {
+		team = team;
+		return;
+
+	}
+	public void CreateTeam() throws RemoteException {
+		for(int i=0;i<=this.getTeam().getMaxSize();i++) {
+			Hashmon bob=new Hashmon(ObjectmonNameGenerator.nextName());
+			team.add(bob);
+		}
+		}
 }
--- GServer.java
+++ GServer.java
@@ -1,4 +1,4 @@
-package lab10;
+package edu.uab.cs203.playground;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
@@ -6,6 +6,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import edu.uab.cs203.Objectmon;
 import edu.uab.cs203.Team;
@@ -23,110 +24,108 @@ public class GServer extends UnicastRemoteObject implements NetworkGym, GymServe
 	private Team teamb;
 	private boolean teamaisready;
 	private boolean teambisready;
-	
+
 
 	protected GServer() throws RemoteException {
 		this.clients = new ArrayList<>();
-		// TODO Auto-generated constructor stub
 	}
-	
-	
-public static void main(String[] arg0) throws RemoteException {
-	try {
-		GServer server = new GServer();
-        Runtime.getRuntime().exec("rmiregistry 10001");
-        Registry registry = LocateRegistry.createRegistry(10001);
-        registry.bind("GServer", server);
-	}catch(Exception e)
-	{System.out.println("Error: "+e.toString());
-	e.printStackTrace();
-	System.exit(0);
-		
+
+
+	public static void main(String[] arg0) throws RemoteException {
+		try {
+			GServer server = new GServer();
+			Runtime.getRuntime().exec("rmiregistry 10001");
+			Registry registry = LocateRegistry.createRegistry(10001);
+			registry.bind("GServer", server);
+		}catch(Exception e)
+		{System.out.println("Error: "+e.toString());
+		e.printStackTrace();
+		System.exit(0);
+
+		}
+		System.out.println("Ready to Begin");
+		return;
 	}
-	System.out.println("Ready to Begin");
-	return;
-}
 
 	@Override
 	public void executeTurn() {
-		// TODO Auto-generated method stub
 		try {
 			this.GClientA.networkTick();
 			this.GClientB.networkTick();
-			 System.out.println(GClientA.nextObjectmon());
-			 System.out.println(GClientB.nextObjectmon());
+			System.out.println(GClientA.nextObjectmon());
+			System.out.println(GClientB.nextObjectmon());
 			Objectmon teama = GClientA.nextObjectmon();
 			Objectmon teamb = GClientB.nextObjectmon();
-			
+
 			if(teama == null &&teamb== null) {
 				return;
 			}
 			this.GClientB.networkApplyDamage(teama, teamb,teama.nextAttack().getDamage(teamb));
+			GClientB.addStatusEffectFromAttack(teama.nextAttack(), teamb);
 			if(teamb.isFainted()==true) {
 				return;	}
+			GClientB.addStatusEffectFromAttack(teamb.nextAttack(), teama);
 			this.GClientA.networkApplyDamage(teamb, teama, teamb.nextAttack().getDamage(teama));
 			return;
-	}catch(Exception e) {
-		System.out.println("Error: "+e.toString());
-		e.printStackTrace();
-	}
+		}catch(Exception e) {
+			System.out.println("Error: "+e.toString());
+			e.printStackTrace();
+		}
 	}
 	@Override
 	public void fight(int arg0) {
-		// TODO Auto-generated method stub
-		  int count = 0;
-		  int end = 50;
-	        while(end != count) {
-	        	if(this.getTeamA().canFight() && this.getTeamB().canFight()) {
-	        	   
-	        	       try {
-        	    	   System.out.print(this.GClientA.getTeam().toString()+"\n");
-						System.out.print(this.GClientB.getTeam().toString()+"\n");
-					} catch (RemoteException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	        		this.executeTurn();
-	        		count ++;
-	        		if(this.getTeamA().canFight() && this.getTeamB().canFight()) {
-	            		this.broadcastMessage("Team A wint after "+count+" rounds.");
-	            		count = end;
-	            	}else if(this.getTeamB().canFight() && this.getTeamA().canFight()){
-	            		this.broadcastMessage("Team B wint after "+count+" rounds.");
-	            		count = end;
-	            	}
-	       
-	        }
-	        	
-	        	if(this.getTeamA().canFight() && this.getTeamB().canFight()) {
-	        		this.broadcastMessage("Draw");
-	        	}
-	        return;
+		int count = 0;
+		int end = 50;
+		while(end != count) {
+			if(this.getTeamA().canFight() && this.getTeamB().canFight()) {
 
-	        
-	    }
+				try {
+					System.out.print(this.GClientA.getTeam().toString()+"\n");
+					System.out.print(this.GClientB.getTeam().toString()+"\n");
+				} catch (RemoteException e) {
+		
+					e.printStackTrace();
+				}
+				this.executeTurn();
+				count ++;
+				if(this.getTeamA().canFight() && this.getTeamB().canFight()) {
+					this.broadcastMessage("Team A wint after "+count+" rounds.");
+					count = end;
+				}else if(this.getTeamB().canFight() && this.getTeamA().canFight()){
+					this.broadcastMessage("Team B wint after "+count+" rounds.");
+					count = end;
+				}
+
+			}
+
+			if(this.getTeamA().canFight() && this.getTeamB().canFight()) {
+				this.broadcastMessage("Draw");
+			}
+			return;
+
+
+		}
 	}
 
 	@Override
 	public GymClient getClientA() {
-		// TODO Auto-generated method stub
+	
 		return GClientA;
 	}
 
 	@Override
 	public GymClient getClientB() {
-		// TODO Auto-generated method stub
+		
 		return GClientB;
 	}
 
 	@Override
 	public Team getTeamA(){
-		// TODO Auto-generated method stub
 		Team zar = null;
 		try {
 			zar = GClientA.getTeam();
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 		return zar;
@@ -134,12 +133,11 @@ public static void main(String[] arg0) throws RemoteException {
 
 	@Override
 	public Team getTeamB(){
-		// TODO Auto-generated method stub
 		Team zar = null;
 		try {
 			zar = this.getClientB().getTeam();
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		return zar;
@@ -147,48 +145,69 @@ public static void main(String[] arg0) throws RemoteException {
 
 	@Override
 	public String networkToString() throws RemoteException {
-		// TODO Auto-generated method stub
 		return this.toString();
 	}
 
 	@Override
-	public void printMessage(String arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		System.out.println(arg0);
-		
+	public void printMessage(String message) throws RemoteException {
+		System.out.println(message);
+		System.out.println(this.teamaisready + "A");
+		System.out.println(this.teambisready + "B");
+		if(teamaisready == true && teambisready != true) {
+			GClientA.printMessage("\nPlease wait as Team B connects." + "\n" + "The fight will begin shortly");
+		}else if(teamaisready != true && teambisready == true) {
+			GClientB.printMessage("\nPlease wait as Team B connects." + "\n" + "The fight will begin shortly");
+		}else if(teamaisready == true && teambisready == true) {
+			GClientA.printMessage("\nPlease look at the server for commands");
+			GClientB.printMessage("\nPlease look at the server for commands");
+			Scanner f = new Scanner(System.in);
+			System.out.println("\n\nAre you ready to begin.");
+			String go = f.nextLine();
+			if(go.equals("yes")) {
+				GClientA.setTeam(teama);
+				System.out.println(this.getTeamA().toString());
+				System.out.println(this.getTeamB().toString());
+			}else {
+				System.exit(0);
+			}
+			System.out.print("/nHow many rounds would you like to go?");
+			int rounds = f.nextInt();
+			this.fight(rounds);
+		}
+
 	}
 
 	@Override
-	public void registerClientA(String arg0, int arg1, String arg2) throws RemoteException {
-		// TODO Auto-generated method stub
-		System.out.println(arg0+arg1+arg2);
+	public void registerClientA(String host, int port, String registryname) throws RemoteException {
+		
+		System.out.println(host+port+registryname);
 		GymClient clientA;
 		try {
-        this.GClientA  = (GymClient) LocateRegistry.getRegistry(arg0, arg1).lookup(arg2);
-        this.clients.add(this.GClientA);
-        this.GClientA.printMessage("Client A registered.");
-        System.out.print(this.GClientA.getTeam().toString()+"\n");
-        this.teamaisready=true;
-        
+			this.GClientA  = (GymClient) LocateRegistry.getRegistry(host, port).lookup(registryname);
+			this.clients.add(this.GClientA);
+			this.GClientA.printMessage("Client A registered.");
+			System.out.print(this.GClientA.getTeam().toString()+"\n");
+			this.teamaisready=true;
+
 		}catch(Exception e) {
 			System.out.println("Error: "+e.toString());
 			e.printStackTrace();
 		}
 		if(this.teambisready){
 			fight(50);}
-		}
+	}
 
 	@Override
-	public void registerClientB(String arg0, int arg1, String arg2) throws RemoteException {
-		// TODO Auto-generated method stub
-		System.out.println(arg0+arg1+arg2);
+	public void registerClientB(String host, int port, String registryname) throws RemoteException {
+		
+		System.out.println(host+port+registryname);
 		GymClient clientB;
 		try {
-        this.GClientB  = (GymClient) LocateRegistry.getRegistry(arg0, arg1).lookup(arg2);
-        this.clients.add(this.GClientB);
-        this.GClientB.printMessage("Client A registered.");
-        System.out.print(this.GClientB.getTeam().toString()+"\n");
-        this.teambisready=true;
+			this.GClientB  = (GymClient) LocateRegistry.getRegistry(host, port).lookup(registryname);
+			this.clients.add(this.GClientB);
+			this.GClientB.printMessage("Client A registered.");
+			System.out.print(this.GClientB.getTeam().toString()+"\n");
+			this.teambisready=true;
 		}catch(Exception e) {
 			System.out.println("Error: "+e.toString());
 			e.printStackTrace();
@@ -199,28 +218,26 @@ public static void main(String[] arg0) throws RemoteException {
 	}
 
 	@Override
-	public void setTeamA(Team arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		this.getClientA().setTeam(arg0);
+	public void setTeamA(Team teama) throws RemoteException {
+		
+		this.getClientA().setTeam(teamb);
 	}
 
 	@Override
-	public void setTeamAReady(boolean arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+	public void setTeamAReady(boolean ready) throws RemoteException {
+
 	}
 
 	@Override
-	public void setTeamB(Team arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		this.getClientB().setTeam(arg0);
-		
+	public void setTeamB(Team teamb) throws RemoteException {
+	
+		this.getClientB().setTeam(teamb);
+
 	}
 
 	@Override
-	public void setTeamBReady(boolean arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+	public void setTeamBReady(boolean ready) throws RemoteException {
+
 	}
 
 }
